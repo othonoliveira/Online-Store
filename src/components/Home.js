e822660c92bf8da2c972ce46c1f743395523c095
Ellen Kecia <ellenkeciaas@gmail.com>
2022-10-07 17:11:05 -0300

    realizacao do requisito 04

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,66 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      category: [],
+    };
+  }
+
+  componentDidMount() {
+    this.imputCategory();
+  }
+
+  onInputChange = (event) => {
+    const { name, type, checked } = event.target;
+    const value = type === 'radio' ? checked : event.target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  imputCategory = async () => {
+    const searchCategory = await getCategories();
+    console.log(searchCategory);
+    this.setState({
+      category: searchCategory,
+    });
+  };
+
   render() {
+    const { category } = this.state;
     return (
-      <h1
-        data-testid="home-initial-message"
-      >
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </h1>
+      <div>
+        <div>
+          <h2>Categorias</h2>
+          {category.map((element, index) => (
+            <div
+              key={ index }
+            >
+              <label htmlFor="categorias" data-testid="category">
+                <input
+                  type="radio"
+                  value={ element.name }
+                  onChange={ this.onInputChange }
+                />
+                {' '}
+                {element.name}
+              </label>
+            </div>
+          ))}
+        </div>
+        <div>
+
+          <h1
+            data-testid="home-initial-message"
+          >
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h1>
+        </div>
+      </div>
     );
   }
 }
