708e80ed43e6101cc1531feddae27c7d934d7991
Beatriz <beatriz.mediatto2@gmail.com>
2022-10-07 16:42:32 -0300

    um pequeno erro

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,18 +4,11 @@ import { Link } from 'react-router-dom';
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <h1
-          data-testid="home-initial-message"
-        >
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1>
-        <Link to="/shoppingcart" data-testid="shopping-cart-button">
-          <button type="button">
-            Carrinho de compras
-          </button>
-        </Link>
-      </div>
+      <h1
+        data-testid="home-initial-message"
+      >
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h1>
     );
   }
 }
